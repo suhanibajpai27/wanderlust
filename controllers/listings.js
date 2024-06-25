628846edaffee91295d0c3ffe6c6b11bf55f8620
Suhani Bajpai <155804539+suhanibajpai27@users.noreply.github.com>
2024-06-26 01:44:43 +0530

    Update listings.js

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,7 +1,7 @@
 const Listing=require("../models/listing.js");
 const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
 const mapToken = process.env.MAP_TOKEN;
-const geocodingClient = mbxGeocoding({ accessToken: MAP_TOKEN });
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 
 module.exports.index=async (req, res) => {
