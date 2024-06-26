b35a30b342ad397c43aee1f6c575795e1483a6b7
Suhani Bajpai <155804539+suhanibajpai27@users.noreply.github.com>
2024-06-26 18:55:38 +0530

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -1,4 +1,7 @@
- mapboxgl.accessToken = mapToken;
+const mapToken = mapToken;
+const listing=listing;
+
+mapboxgl.accessToken = mapToken;
       const map = new mapboxgl.Map({
           container: 'map', // container ID
           center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
